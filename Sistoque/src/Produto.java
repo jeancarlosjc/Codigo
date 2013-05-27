8521e093683b1565e078a0a33b8ce0ec99e39341
Ademar Francisco da Silva Júnior <ademarfsj@gmail.com>
2013-05-27 01:56:41 -0300

    Classe Produto adicionada

---
+++ Sistoque/src/Produto.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ademar
+ */
+public class Produto {
+    
+    conexao.Conexao conexao = new conexao.Conexao();
+    String id;
+    private String nome;
+    private String descricao;
+    private int quantidade;
+    private float precoVenda;
+    private float precoCompra;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(float precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public float getPrecoCompra() {
+        return precoCompra;
+    }
+
+    public void setPrecoCompra(float precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+    
+    
+    
+}
